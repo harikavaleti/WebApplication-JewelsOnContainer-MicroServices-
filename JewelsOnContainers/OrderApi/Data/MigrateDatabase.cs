030c30f366190701ea57148ba8a94fa3855a5700
harikavaleti <harikavaleti@gmail.com>
2020-05-01 19:41:47 -0700

    Added Order API

---
+++ JewelsOnContainers/OrderApi/Data/MigrateDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderApi.Data
+{
+    public class MigrateDatabase
+    {
+        public static void EnsureCreated(OrdersContext context)
+        {
+            context.Database.Migrate();
+        }
+    }
+}
---
+++ JewelsOnContainers/OrderApi/Models/OrderItem.cs
@@ -0,0 +1,52 @@
+﻿using OrderApi.Infrastructure.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApi.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Units { get; set; }
+        public int ProductId { get; set; }
+
+        protected OrderItem() { }
+        
+        public Order Order { get; set; }
+        public int OrderId { get; set; }
+
+        public OrderItem(int productId, string productName, decimal unitPrice, string pictureUrl, int units = 1)
+        {
+            if(units <= 0)
+            {
+                throw new OrderingDomainException("Invalid number of Units");
+            }
+
+            ProductId = productId;
+
+            ProductName = productName;
+            UnitPrice = unitPrice;
+
+            Units = units;
+            PictureUrl = pictureUrl;
+        }
+
+        public void SetPictureUri(string picUri)
+        {
+            if (!String.IsNullOrWhiteSpace(picUri))
+            {
+                PictureUrl = picUri;
+            }
+        }
+    }
+}
